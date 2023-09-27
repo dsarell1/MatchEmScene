b18ef6280ed2e63f8cb45c97e27ee550c8445b9c
dsarell1 <dsarell1@kent.edu>
2023-09-26 21:53:56 -0400

    Small Progress on Matching Game. duplicate Rectangles are now printed.

--- A1-MatchingGame/GameSceneViewController.swift
+++ A1-MatchingGame/GameSceneViewController.swift
@@ -11,50 +11,57 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var scoreTimerLabel: UILabel!
+    
+    @IBOutlet weak var startBtn: UILabel!
+    
     var rectButtons: [UIButton]?
     
-    var matchRect: [UIButton: Int]? // Dictionary for Matching
+    var matchRect: [UIButton: Int] = [:] // Dictionary for Matching
     
-    var gameTimerInterval: TimeInterval = 12.0
+    var gameTimerInterval: TimeInterval = 1.0
     var newRectInterval: TimeInterval = 1.0
     var gameTimer: Timer?
     var newRectTimer: Timer?
     
-    var buttonTag = 0
-    var rectCount = 0
-    var score = 0
+    var gameRunning = false
+    
+    var buttonTag = 1
+    var rectCount = 0 {
+        didSet {
+            self.updateLabel()
+        }
+    }
+    var score = 0 {
+        didSet {
+            self.updateLabel()
+        }
+    }
     var timeRemaining = 12 {
         didSet {
-            self.scoreTimerLabel.text = "Created \(self.rectCount) - Time: \(self.timeRemaining) - Score: \(self.score)"
+           // self.scoreTimerLabel.text = "Created \(self.rectCount) - Time: \(self.timeRemaining) - Score: \(self.score)"
+            self.updateLabel()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello World")
-        print("Hello GitHub!!!")
         //self.rectButtons = []
     }
-    /*
-    override func viewDidAppear(_ animated: Bool) {
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-        self.createButton()
-    }*/
+
     @IBAction func startButton(_ sender: UIButton) {
-        StartGame()
-        self.createButton()
-        self.createButton()
-        self.createButton()
+        if self.gameRunning == false {
+            self.gameRunning = true
+            //startBtn.text = "reset"
+            sender.removeFromSuperview()
+            StartGame()
+        }
+        else {
+            //let startBtn = UIButton()
+            self.view.addSubview(sender) //add back to superview
+            //startBtn.text = "start"
+            timeRemaining = 12;
+        }
     }
     
     func StartGame() {
@@ -66,39 +73,77 @@ class ViewController: UIViewController {
                 self.newRectTimer?.invalidate()
                 self.view.backgroundColor = .red
                 Timer.invalidate()
+                self.gameRunning = false
+                //self.view.addSubview(self.startBtn)
             }
         })
         self.newRectTimer = Timer.scheduledTimer(withTimeInterval: newRectInterval, repeats: true, block: { Timer in
             self.createButton()
+            self.rectCount += 2
         })
     }
-    func createButton() {
+    func updateLabel() {
+        self.scoreTimerLabel.text = "Created \(self.rectCount) - Time: \(self.timeRemaining) - Score: \(self.score)"
+    }
+    func createButton() { // Creates 2 Rectangles with the same size and color but in random locations.
         let rectSize = self.randSize()
         let rectloc = self.randLocation(rectSize)
-        let rectFrame = CGRect(origin: rectloc, size: rectSize)
+        let rectMatch = CGRect(origin: rectloc, size: rectSize)
+        let rectloc2 = self.randLocation(rectSize)
+        let rectMatch2 = CGRect(origin: rectloc2, size: rectSize)
         
-        let button = UIButton(frame: rectFrame)
-        button.addTarget(self, action: #selector(handleTap(_:)), for: .touchUpInside)
-        button.backgroundColor = self.randColor()
-        button.tag = self.buttonTag
-        //self.button += 1
-        self.view.addSubview(button)
-        self.rectButtons?.append(button)
+        let buttonMatch = UIButton(frame: rectMatch)
+        let buttonMatch2 = UIButton(frame: rectMatch2)
+        
+        buttonMatch.addTarget(self, action: #selector(handleTap(_:)), for: .touchUpInside)
+        buttonMatch2.addTarget(self, action: #selector(handleTap(_:)), for: .touchUpInside)
+        
+        let matchColor = self.randColor()
+        buttonMatch.backgroundColor = matchColor
+        buttonMatch2.backgroundColor = matchColor
+        
+        buttonMatch.tag = self.buttonTag
+        buttonMatch2.tag = self.buttonTag
+        self.buttonTag += 1
+        self.view.addSubview(buttonMatch)
+        self.view.addSubview(buttonMatch2)
+        self.rectButtons?.append(buttonMatch)
+        self.rectButtons?.append(buttonMatch2)
     }
     @objc func handleTap(_ sender: UIButton) {
-        print(sender.tag)
-        sender.removeFromSuperview()
-        self.score += 1
+        if self.gameRunning == true {
+            //print(sender.tag)
+            sender.setTitle("@", for: .normal)
+            //sender.removeFromSuperview()
+            self.score += 1
+            //print(self.rectButtons[sender])
+            //let matchButton: UIButton? = UIButton()
+            //if let a = matchButton {
+            //    if self.rectButtons[a] == self.rectButtons[sender] {
+            //       a.removeFromSuperview()
+            //       sender.removeFromSuperview()
+            //       self.score += 1
+            //    }
+            //    else {
+            //       matchButton = nil
+            //   }
+            // }
+            // else {
+            //    matchButton = sender
+            //    sender.setTitle("@", for: .normal)
+            // }
+        }
     }
+    
     func randSize() -> CGSize {
-        let width = CGFloat.random(in: 50.0...150.0)
-        let height = CGFloat.random(in: 50.0...150.0)
+        let width = CGFloat.random(in: 25.0...100.0)
+        let height = CGFloat.random(in: 25.0...100.0)
         return CGSize(width: width, height: height)
     }
     func randLocation(_ rectSize: CGSize) -> CGPoint {
         let x = CGFloat.random(in: 0...(self.view.frame.width - (rectSize.width/2)))
-        let topSafeInset = self.view.safeAreaInsets.bottom
-        let bottomSafeInset = self.view.safeAreaInsets.top
+        let topSafeInset = self.view.safeAreaInsets.bottom + 60
+        let bottomSafeInset = self.view.safeAreaInsets.top + 70
         let y = CGFloat.random(in: (bottomSafeInset + (rectSize.height/2))...(self.view.frame.height - topSafeInset - (rectSize.height/2)))
         return CGPoint(x: x, y: y)
     }
